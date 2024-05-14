6fefc915aeb28f6761066484f02a57664142bd6f
Srivastava-Kaushal <21je0459@iitism.ac.in>
2024-05-14 22:33:33 +0530

    Added user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,13 +6,3 @@ const asynHandler=(requestHandler)=>{
 }
 
 export {asynHandler}
-// const asynHandler=(fn)=>async (req,res,next)=>{
-//     try{
-//         await fn(req,res,next)
-//     }catch(err){
-//         res.status(err.code||500).json({
-//             success:false,
-//             message:err.message||"Internal Server Error"
-//         })
-//     }
-// }
