c05cc32db73bd876f45ae6254a0194c7fa34d0e2
Srivastava-Kaushal <21je0459@iitism.ac.in>
2024-05-15 19:20:28 +0530

    cloudinary and multer added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploaderOnCloudinary= async(localFilePath) =>{
+    try{
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        });
+        console.log("File is uploaded on cloudinary",response.url);
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+export {uploaderOnCloudinary}
