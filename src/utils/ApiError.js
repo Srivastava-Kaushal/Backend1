7035fdbaf032a1f32e80c91cf0925fea9a1a7dcc
Srivastava-Kaushal <21je0459@iitism.ac.in>
2024-05-15 18:22:10 +0530

    correction

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         message="Something went wrong",
         statusCode,
         errors=[],
-        statck=""
+        stack=""
     ) {
         super(message);
         this.statusCode = statusCode;
@@ -11,8 +11,8 @@ class ApiError extends Error {
         this.message=false;
         this.success=false;
         this.errors=errors;
-        if(statck){
-            this.statck=statck;
+        if(stack){
+            this.stack=stack;
         }
         else{
             Error.captureStackTrace(this, this.constructor);
